085b0ddc477c818562609dcc1f72c2e952c2e28f
5004 <5004>
2017-08-24 18:36:44 +0900

    actiity add 추가

--- src/main/java/com/hanbit/there/api/admin/controller/AdminActivityController.java
+++ src/main/java/com/hanbit/there/api/admin/controller/AdminActivityController.java
@@ -6,10 +6,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hanbit.there.api.admin.service.AdminActivityService;
 import com.hanbit.there.api.domain.Activity;
 
@@ -19,6 +23,8 @@ public class AdminActivityController {
 	
 	@Autowired
 	private AdminActivityService adminActivityService;
+	
+	private ObjectMapper mapper = new ObjectMapper();
 	
 	@RequestMapping("/{thereId}/activities")
 	public List<Activity> getActivities(@PathVariable("thereId")String thereId) {
@@ -31,6 +37,18 @@ public class AdminActivityController {
 		
 		Map result = new HashMap();
 		result.put("exists", exists);
+		
+		return result;
+	}
+	
+	@PostMapping("/activity/save") // @RequestMapping의 method=RequestMethod.POST 안써도됨
+	public Map saveActivity(@RequestParam("model") String model, @RequestParam("photos") List<MultipartFile> photos) throws Exception { // List<MultipartFile> 파일들 list로 받으면됨 - request 할 필요 없음
+		Activity activity = mapper.readValue(model, Activity.class);
+		
+		adminActivityService.saveActivity(activity, photos);
+		
+		Map result = new HashMap();
+		result.put("status", "OK");
 		
 		return result;
 	}
--- src/main/java/com/hanbit/there/api/admin/service/AdminActivityService.java
+++ src/main/java/com/hanbit/there/api/admin/service/AdminActivityService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.hanbit.there.api.admin.reop.AdminActivityRepository;
 import com.hanbit.there.api.domain.Activity;
+import com.hanbit.there.api.service.FileService;
 
 @Service
 public class AdminActivityService {
@@ -14,11 +16,20 @@ public class AdminActivityService {
 	@Autowired
 	private AdminActivityRepository adminActivityRepository;
 	
+	@Autowired
+	private FileService fileService;
+	
 	public List<Activity> getActivities(String thereId) {
 		return adminActivityRepository.findByThereIdOrderByName(thereId);
 	}
 	
 	public boolean hasActivityId(String id) {
 		return adminActivityRepository.exists(id);
+	}
+	
+	public void saveActivity(Activity activity, List<MultipartFile> photos) {
+		// TODO 파일처리
+		
+		adminActivityRepository.save(activity);
 	}
 }
